5fc86856b068fcd0427b4e80932fa29b0881ebac
lukas-nietsch <64492669+lukas-nietsch@users.noreply.github.com>
2024-07-04 16:55:29 +0200

    v0.2 - implement DatePicker and update when date selected

--- js/map_01.js
+++ js/map_01.js
@@ -1,8 +1,7 @@
-//declare map variable globally so all functions have access
-
+// Declare map variable globally so all functions have access
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///// Create basic map
-var map = L.map('map').setView([51.125, 10.375], 6)
+// Create basic map
+var map = L.map('map').setView([51.125, 10.375], 6);
 
 // Basemaps
 var OSM = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,17 +9,19 @@ var OSM = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var OSM_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 }).addTo(map);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///// FUNCTIONS
-
+// FUNCTIONS
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///// Add GeoJSON Data - Kreise with mean r0 values
-var json_path = '/data/r0_2019.geojson'
+// Add GeoJSON Data - Kreise with mean r0 values
+var json_path = '/data/r0_2019.geojson';
+
+var jsonLayer;
+var selectedDay = 1; // default to the first day of the year
 
 // Fetch the GeoJSON data and add it to the map
 fetch(json_path)
@@ -31,9 +32,11 @@ fetch(json_path)
         return response.json();
     })
     .then(data => {
-        var jsonLayer = L.geoJSON(data, {
+        jsonLayer = L.geoJSON(data, {
             onEachFeature: function (feature, layer) {
-                layer.bindPopup(`<b>Landkreis: </b>` + feature.properties.NAME_3);
+                layer.on('click', function (e) {
+                    updatePopup(feature, layer);
+                });
             },
             style: {
                 fillColor: 'red',
@@ -45,7 +48,7 @@ fetch(json_path)
 
         // Add to overlayMaps
         var overlayMaps = {
-            "Raster Image": img,
+            "Raster Image": imgLayer,
             "Landkreisgrenzen": jsonLayer
         };
 
@@ -56,24 +59,68 @@ fetch(json_path)
         console.error('Error fetching the GeoJSON data:', error);
     });
 
+function updatePopup(feature, layer) {
+    var attributeName = 'mn__' + selectedDay;
+    var attributeValue = feature.properties[attributeName]
+    // Check if the attribute value is a number and round it to 2 decimal places
+    if (attributeValue !== undefined && !isNaN(attributeValue)) {
+        attributeValue = attributeValue.toFixed(2);
+    } else {
+        attributeValue = 'N/A';
+    }
+    layer.bindPopup(`<b>Landkreis: </b>${feature.properties.NAME_3}<br><b>Mean R0: </b>${attributeValue}`).openPopup();
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///// Add Raster Images as png ImageOverlay - NOTE: All files must have the exact same Extent!
+// Add Raster Images as png ImageOverlay - NOTE: All files must have the exact same Extent!
 
 // Extents of the Germany Raster: 
-var ext_ger = [[47.2500000000000000, 5.7500000000000000], [55.0000000000000000, 15.0000000000000000]]
+var ext_ger = [[47.25, 5.75], [55.00, 15.00]];
 // Extents of the Bavaria Raster: 
-var ext_bav = [[46.7187500000000000, 8.4687500000000000], [51.0937500000000000, 14.3437500000000000]]
+var ext_bav = [[46.71875, 8.46875], [51.09375, 14.34375]];
 
-var img_path = '/data/png/colored_img_216.png'
+var imgLayer;
 
-var img = L.imageOverlay(image = img_path, 
-    bounds = ext_bav,
-    opacity = 0.8).addTo(map)
+// Function to update the image overlay based on the selected date
+function updateImage(date) {
+    selectedDay = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 86400000);
+    var imgPath = `/data/png/colored_img_${selectedDay}.png`;
+
+    if (imgLayer) {
+        map.removeLayer(imgLayer);
+    }
+
+    imgLayer = L.imageOverlay(imgPath, ext_bav, { opacity: 0.8 }).addTo(map);
+
+    // Update all popups with the new day
+    if (jsonLayer) {
+        jsonLayer.eachLayer(function (layer) {
+            updatePopup(layer.feature, layer);
+        });
+    }
+}
+
+// Initialize the DatePicker with todays date as the default
+$(function() {
+    var today = new Date();
+    var formattedDate = today.toISOString().split('T')[0];
+    $("#datepicker").datepicker({
+        dateFormat: "yy-mm-dd",
+        onSelect: function(dateText) {
+            var date = new Date(dateText);
+            updateImage(date);
+        }
+    }).datepicker("setDate", formattedDate);
+
+    // Update the map with today's date initially
+    updateImage(today);
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///// Layer Controller
+// Layer Controller
 var baseMaps = {
     "Open Street Map": OSM,
     "Open Street Map - Hot": OSM_HOT
 };
+
+// The overlayMaps creation is moved into the fetch block since it relies on the jsonLayer being defined after fetching data
