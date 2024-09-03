94aa39075ffa7bc67b769cd5826ec03505471a6f
lukas-nietsch <64492669+lukas-nietsch@users.noreply.github.com>
2024-09-03 10:41:29 +0200

    adjust close/openBtns

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,32 @@
 function openNav(){
     document.getElementById("mySidepanel").style.width = "300px";
-    document.getElementById("map").style.marginLeft = "300px";
-    document.getElementById("myFooter").style.marginLeft = "300px";
-    document.getElementById("h1-header").style.marginLeft = "260px";
+    document.getElementById("mySidepanel").style.padding = "20px";
+    document.getElementById("map").style.marginLeft = "350px";
+    document.getElementById("myFooter").style.marginLeft = "350px";
+/*     document.getElementById("openbtn").style.display = 'none'; */
+/*     document.getElementById("closebtn").style.display = 'block'; */
+    document.getElementById("myMain").style.marginLeft = "350px";
+    document.getElementById("myCloseButton").style.zIndex = "3";
+    document.getElementById("header").style.paddingLeft = "0px";
 }
 function closeNav(){
     document.getElementById("mySidepanel").style.width = "0px";
+    document.getElementById("mySidepanel").style.padding = "0px";
     document.getElementById("map").style.marginLeft ="0px";
     document.getElementById("myFooter").style.marginLeft ="0px";
-    document.getElementById("h1-header").style.marginLeft = "30px";
+/*     document.getElementById("closebtn").style.display = 'none';
+    document.getElementById("openbtn").style.display = 'block'; */
+    document.getElementById("myMain").style.marginLeft = "0px";
+    document.getElementById("myCloseButton").style.zIndex = "0";
+    document.getElementById("header").style.paddingLeft = "80px";
+}
+
+function sidebarBtn(){
+    var x = document.getElementById("myMain");
+    if (x.style.marginLeft === "350px"){
+        closeNav();
+        console.log("close", x.style.marginLeft)
+    }else{
+        openNav();
+    }
 }
