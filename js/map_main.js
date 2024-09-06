9e96425aba8b04c80507f7239ca2be157736f73d
lukas-nietsch <64492669+lukas-nietsch@users.noreply.github.com>
2024-09-06 15:11:01 +0200

    update

--- js/map_main.js
+++ js/map_main.js
@@ -3,16 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     var map = L.map('map').setView([51.1657, 10.4515], 6); // Centered on Germany
 
     // Add OpenStreetMap tile layer
-/*     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-    }).addTo(map); */
+    }).addTo(map);
     
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+
+    // This basemap is not working online
+/*     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
         minZoom: 0,
         maxZoom: 20,
         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         ext: 'png'
-    }).addTo(map);
+    }).addTo(map); */
 
     // Function to get the day of the year
     function getDayOfYear(date) {
