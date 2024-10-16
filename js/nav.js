54ca77cb1bcc475892c7ab3770df8ef9c16fea59
lukas-nietsch <64492669+lukas-nietsch@users.noreply.github.com>
2024-10-16 08:33:32 +0200

    Deploy new responsive Version and Design.

--- js/nav.js
+++ js/nav.js
@@ -1,30 +1,6 @@
-function openNav(){
-    document.getElementById("leftSidepanel").style.width = "300px";
-    document.getElementById("leftSidepanel").style.padding = "20px";
-    document.getElementById("map").style.marginLeft = "350px";
-    document.getElementById("myFooter").style.marginLeft = "350px";
-    document.getElementById("myMain").style.marginLeft = "350px";
-    document.getElementById("myCloseButton").style.zIndex = "3";
-    document.getElementById("header").style.paddingLeft = "0px";
-    document.getElementById("chart-container").style.zIndex = "3";
-}
+// Collapsable Sidebar
+const toggler = document.querySelector(".btn");
+toggler.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
 
-function closeNav(){
-    document.getElementById("leftSidepanel").style.width = "0px";
-    document.getElementById("leftSidepanel").style.padding = "0px";
-    document.getElementById("map").style.marginLeft ="0px";
-    document.getElementById("myFooter").style.marginLeft ="0px";
-    document.getElementById("myMain").style.marginLeft = "0px";
-    document.getElementById("myCloseButton").style.zIndex = "0";
-    document.getElementById("header").style.paddingLeft = "80px";
-    document.getElementById("chart-container").style.zIndex = "0";
-}
-
-function sidebarBtn(){
-    var x = document.getElementById("myMain");
-    if (x.style.marginLeft === "350px"){
-        closeNav();
-    }else{
-        openNav();
-    }
-}
